add get_params and fix overflow issue

neuralib/architectures.py
@@ -75,7 +75,12 @@ class Architecture(ABC):
         
         return True
     
-
+    @abstractmethod
+    def get_params(self) -> List[dict[str, np.array]]:
+        """
+        Return a list of the parameters of each layer in the model.
+        """
+        pass
 
 
 class Model(Architecture):
@@ -182,6 +187,13 @@ class Model(Architecture):
                 gradient = layer.backward()
                 continue
             gradient = layer.backward(gradient)
+
+    def get_params(self) -> List[dict[str, np.array]]:
+        """
+        Return a list of the parameters of each layer in the sequential model.
+        """
+        super().get_params()
+        return [layer.get_params() for layer in self.layers if isinstance(layer, GradLayer)]
 
     def plot_progress(self):
         # Plot training error progression over time

neuralib/layers/layers.py
@@ -57,7 +57,8 @@ class GradLayer(ComputationalLayer):
         self.d_biases = np.zeros(self.biases.shape) # 1 x n_outputs = biases.shape
 
     def update(self, optimizer: Optimizer) -> None:
-        """Update the weights and biases of the layer using the gradients computed 
+        """
+        Update the weights and biases of the layer using the gradients computed 
         during the backward pass and an optimizer of choice.
 
         Args:
@@ -66,6 +67,13 @@ class GradLayer(ComputationalLayer):
         """
         self.weights += optimizer.step(self.d_weights)
         self.biases += optimizer.step(self.d_biases)
+
+    def get_params(self) -> dict[str, np.array]:
+        """
+        Method that returns all the parameters of the layer (weights and biases) in a dictionary
+        as well as the number of parameters in the layer.
+        """ 
+        return {'weights': self.weights, 'biases': self.biases, 'n_params': self.weights.size + self.biases.size}
 
 
 class Linear(GradLayer):

neuralib/layers/losses.py
@@ -1,3 +1,4 @@
+from cmath import isnan
 import numpy as np
 from neuralib.layers.layers import ComputationalLayer
 from typing import Union
@@ -51,7 +52,17 @@ class MSE(Loss):
         super().forward(y_pred, y_true)
 
         residual = y_pred - y_true
-        error = np.sum(residual**2)/(2*y_pred.shape[0])
+        try:
+            error = np.sum(residual**2)/(2*y_pred.shape[0])
+        except OverflowError as err:
+            # Set error to infinity 
+            print('Overflowed after ', error, err)
+            error = np.inf
+        except:
+            raise
+        if isnan(error):
+            error = np.inf
+            
         assert(error >= 0), "Error must be non-negative"
 
         return residual, error  

tests/integration_tests/test_xor_data.py
@@ -1,10 +1,14 @@
 from unittest import TestCase
 
+import warnings
+import pytest
+
 import numpy as np
 
 from neuralib import Model
 from neuralib.layers import Linear
 from neuralib.layers.activations import Sigmoid
+from neuralib.layers.layers import GradLayer
 from neuralib.layers.losses import MSE
 from neuralib.optimizers import VGD
 from neuralib.utils import xor_data
@@ -39,18 +43,36 @@ class XorDataTest(TestCase):
         self.model.train(self.X, self.y, batch_size=4, epochs=10000, optimizer=VGD(lr=0.1))
 
 
-        X_test = np.array([[0,0], 
-                           [0,1], 
-                           [1,0], 
-                           [1,1]])
-        y_test = np.array([[0], [1], [1], [0]])
-        y_pred = self.model.predict(X_test)
+        # Test on training data
+        y_pred = self.model.predict(self.X)
 
         # Check that the model has learned the XOR function
-        self.assertTrue(np.allclose(y_pred, y_test))
-    
-    # TODO: test that without activation the linear model does not learn the XOR function
+        self.assertTrue(np.allclose(y_pred, self.y))
 
+    @pytest.mark.filterwarnings("ignore")
+    def test_xor_without_activation(self):
+        model = self.model
+        
+        # Pop the activation layer
+        model.pop(1)
 
+        print(model.layers)
+        # Train the model
+        model.train(self.X, self.y, batch_size=4, epochs=10000, optimizer=VGD(lr=0.1))
+
+        # Test on training data
+        y_pred = model.predict(self.X)
+
+        # Check that the model has learned the XOR function
+        self.assertFalse(np.allclose(y_pred, self.y))
+
+    def test_get_params(self):
+        params = self.model.get_params()
+        self.assertEqual(len(params), len([l for l in self.model.layers if isinstance(l, GradLayer)]))
+
+        # Compare total number of params. This currently only works because the layers are linear
+        pred_n_params = sum([p['n_params'] for p in params])  
+        n_params = sum([p['weights'].size + p['biases'].size for p in params])
+        self.assertEqual(pred_n_params, n_params)
 
    